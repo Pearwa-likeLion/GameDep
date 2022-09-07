c4bf2bc792ed319c6ad4173ee96c62e8e24ad492
Pearwa Tussanapoom <FITTOKUNG@GMAIL.COM>
2022-09-07 16:54:44 +0700

    first commit

---
+++ setting.py
@@ -0,0 +1,18 @@
+level_map = [
+'                                           ',  
+'                                           ', 
+'                                           ', 
+'XXXXXX           XXXXX                XXXX',
+'XXXXXXXX                  XXXXX        XXXX',    
+'                        XXXX       XXXXXXXX',    
+'                XXXXXXXXXXX                ', 
+'XXXXXXX        XXX       XXXX              ',
+'XXXXX       XXXXXX             XXX     XXXX',
+'XX        XXXXXXXX             XXX     XXXX',
+'        XXXXXXXXXX             XXX     XXXX',
+'XXXXXXXXXXXXXXXXXX             XXX     XXXX'    
+]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map)*tile_size
